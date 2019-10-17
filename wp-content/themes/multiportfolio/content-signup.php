5659e12a0259be7828cf246dfafaec91a26a4ab9
Alex Mattingley <alexmattingley@gmail.com>
2019-10-17 14:06:53 -0700

    updated lotus image to play nicely with mobile. Updated the content sign up to use different plugin to reduce error rate

--- wp-content/themes/multiportfolio/content-signup.php
+++ wp-content/themes/multiportfolio/content-signup.php
@@ -7,21 +7,7 @@
 					<p>Receive meaningful content and course updates straight to your inbox</p>
 				</div>
 				<div id="mc_embed_signup">
-					<form id="mc-embedded-subscribe-form" action="http://karenkreddingphd.us8.list-manage.com/subscribe/post" method="post">
-						<input type="hidden" name="u" value="89a20367cc6d90468b56d89d9">
-						<input type="hidden" name="id" value="a596cf6d20">
-						<div id="mc_embed_signup_scroll">
-							<div class="mc-field-group">
-								<input id="mce-EMAIL" class="required email" name="EMAIL" type="email" placeholder="email" />
-							</div>
-							<div class="mc-field-group">
-								<input id="mce-FNAME" class="" name="FNAME" type="text" placeholder="name" />
-							</div>
-							<!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-							<div style="position: absolute; left: -5000px;"><input tabindex="-1" name="b_89a20367cc6d90468b56d89d9_a596cf6d20" type="text" value="" /></div>
-							<button id="mc-embedded-subscribe" class="button" name="subscribe" type="submit" />Sign Me Up!</button>
-						</div>
-					</form>
+					<?php the_field('newsletter_form'); ?>
 				</div>
 			</div>
 		</div>
